b9093f72ebf7ed06bf66c76fc92473fa097eb7f6
piyushabbi <piyushabbi27@gmail.com>
2018-06-13 20:35:18 +0530

    Init Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+
+class App extends Component {
+	render() {
+		return <div className="App">hello</div>;
+	}
+}
+
+export default App;
